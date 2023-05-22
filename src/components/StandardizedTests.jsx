72efef3f1e2565da759fd3c071a366f312b29966
Sabbir Ahmed <sabbirahmed.cuet.ete@gmail.com>
2023-05-22 23:45:17 +0600

    Updated StandardizedTest component layout.

--- src/components/StandardizedTests.jsx
+++ src/components/StandardizedTests.jsx
@@ -1,26 +1,35 @@
-import { Row, Col } from "react-bootstrap";
+import { Fragment } from "react";
+import { Table } from "react-bootstrap";
+import { SiTestcafe } from "react-icons/si";
 import tests from "../data/tests";
 
 const StandardizedTests = () => {
   const sortedTests = tests.sort((a, b) => b.date_taken - a.date_taken);
   return (
     <section>
-      <ul>
-        {sortedTests.map((test) => (
-          <li key={test._id} className="mb-2">
-            <div className="d-flex justify-content-between">
-              <span className="fw-bold text-muted">{test.test_name}</span>
-              <span className="fw-light fst-italic">{test.test_location} </span>
-            </div>
-            <div className="d-flex justify-content-between align-items-center">
-              <span className="fw-light">{test.score}</span>
-              <time className="badge bg-success">
-                {`${test.date_taken.getDate()}/${test.date_taken.getMonth()}/${test.date_taken.getFullYear()}`}
-              </time>
-            </div>
-          </li>
-        ))}
-      </ul>
+      <Table size="sm" borderless>
+        <thead></thead>
+        <tbody>
+          {sortedTests.map((test) => (
+            <Fragment key={test._id}>
+              <tr>
+                <td>
+                  <SiTestcafe size={25} className="text-success" />
+                </td>
+                <td className="fw-bold text-muted">{test.test_name}</td>
+                <td className="fw-light fst-italic">{test.test_location}</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td className="fw-light">{test.score}</td>
+                <td>
+                  <time className="badge bg-success">{`${test.date_taken.getDate()}/${test.date_taken.getMonth()}/${test.date_taken.getFullYear()}`}</time>
+                </td>
+              </tr>
+            </Fragment>
+          ))}
+        </tbody>
+      </Table>
     </section>
   );
 };
