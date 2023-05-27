8d365a5c664687cb3e62137aa5d825d0370ba72d
Sabbir Ahmed <sabbirahmed.cuet.ete@gmail.com>
2023-05-27 14:51:24 +0600

    Updated visitormap and style.

--- src/components/VisitorMap.jsx
+++ src/components/VisitorMap.jsx
@@ -12,8 +12,12 @@ const VisitorMap = () => {
       <div className="section-content">
         <Row className="justify-content-center my-3">
           <Col sm={8} className="text-center">
-            <a href="https://clustrmaps.com/site/1buuw" title="Visit tracker">
-              <img src="//clustrmaps.com/map_v2.png?cl=ffffff&w=300&t=tt&d=XfPN6nLJI6Q1xG_D-Wze9Tdf4FqJERVIzCFeLO4eOR4&co=2d78ad&ct=ffffff" />
+            <a
+              className="visitor-map"
+              href="https://clustrmaps.com/site/1buuw"
+              title="Visit tracker"
+            >
+              <img src="//clustrmaps.com/map_v2.png?cl=105f05&w=300&t=tt&d=XfPN6nLJI6Q1xG_D-Wze9Tdf4FqJERVIzCFeLO4eOR4&co=e1eedd&ct=105f05" />
             </a>
           </Col>
         </Row>
